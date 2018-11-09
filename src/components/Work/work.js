00ba5da0c7c2039b83d4eb343bc12e28cf27ccbf
Kendra Kyndberg <kendrak922@gmail.com>
2018-11-09 16:14:58 -0600

    added projects

--- src/components/Work/work.js
+++ src/components/Work/work.js
@@ -4,37 +4,48 @@ import { graphql } from 'gatsby'
 import chillTime from '../../../public/images/chillTime.png'
 import amazon from '../../../public/images/amazon.png'
 import people from '../../../public/images/people.png'
+import './style.scss';
 
+const Work = () => (
 
-class Work extends React.Component {
-
-    render() {
-        return (
-            <div>
-                <h1>Stuff I've worked on</h1>
-                <div className="workContainer">
-                    <div className="project">
-                        <img className="project-image"src={chillTime} />
-                        <p className="project-title">Chill Time Trivia</p>
-                        <p className="work-description">The app will allow users to log in. Users play a single game of of trivia with friends or random users, trivia games will be interactive including chat. Question will only be in mulitple choice format. No schedules no waiting. Play when you want.</p>
-                    </div>
-                    <div className="project">
-                        <img className="project-image"src={people} />
-                        <p className="project-title">We The People</p>
-                        <p className="work-description">A full-stack Javascript app which allows the user to be more engaged in local politics.</p>
-                    </div>
-                    <div className="project">
-                        <img className="project-image"src={amazon} />
-                        <p className="project-title">Amazon-Customer</p>
-                        <p className="work-description">Node.js and MySQL app</p>
-                    </div>
-                    {/* add banshee */}
+    <div>
+        <h1>Stuff I've worked on</h1>
+        <div className="work-container">
+            <div className="project">
+            <div className="project-left">
+            <p className="project-title">Chill Time Trivia</p>
+                <img className="project-image" src={chillTime} />
+                </div>
+                <div className="project-right">
+                    <p className="work-description">The app will allow users to log in. Users play a single game of of trivia with friends or random users, trivia games will be interactive including chat. Question will only be in mulitple choice format. No schedules no waiting. Play when you want.</p>
+                    <p>Github</p>
                 </div>
             </div>
-        )
-    }
+            <div className="project">
+            <div className="project-left">
+            <p className="project-title">We The People</p>
+                <img className="project-image" src={people} />
+            </div>
+                <div className="project-right">
+                    <p className="work-description">A full-stack Javascript app which allows the user to be more engaged in local politics.</p>
+                     <p>Github</p>
+                </div>
+            </div>
+            <div className="project">
+            <div className="project-left">
+            <p className="project-title">Amazon-Customer</p>
+                <img className="project-image" src={amazon} />
+            </div>
+                <div className="project-right">
+                  <p className="work-description">Node.js and MySQL app</p>
+                  <p>Github</p>
+               </div>
+            </div>
+                    </div>
+                            </div>
 
-}
+
+                            );
 
 export default Work;
 
