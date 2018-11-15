e24710c15b06027cb677e5c1ebfdba87774f6b8f
Kendra Kyndberg <kendrak922@gmail.com>
2018-11-15 16:29:24 -0600

    added links

--- src/components/Work/work.js
+++ src/components/Work/work.js
@@ -18,7 +18,8 @@ const Work = () => (
                 </div>
                 <div className="project-right">
                     <p className="work-description">The app will allow users to login and play any time of the day. Users play a single game of of trivia with friends or random users, trivia games will be interactive including chat. Question will only be in mulitple choice format. No schedules no waiting. Play when you want.</p>
-                    <p className="project-link">Github</p>
+                    <p className="project-link"><a target='_blank' rel="noopener noreferrer" href="https://github.com/finne132/reactionTime">Github</a></p>
+                    <p className="project-link"><a target='_blank' rel="noopener noreferrer"href="https://chilltimetrivia.herokuapp.com">Play Game</a></p>
                 </div>
             </div>
             <div className="project">
@@ -28,7 +29,8 @@ const Work = () => (
             </div>
                 <div className="project-right">
                     <p className="work-description">A full-stack Javascript app which allows the user to be more engaged in local politics.</p>
-                     <p className="project-link">Github</p>
+                     <p className="project-link"><a target='_blank' rel="noopener noreferrer"href="https://github.com/kendrak922/We-the-People">Github</a></p>
+                     <p className="project-link"><a target='_blank' rel="noopener noreferrer"href="https://wethepeopleapp.herokuapp.com/index.html">Check it Out</a></p>
                 </div>
             </div>
             <div className="project">
@@ -38,13 +40,11 @@ const Work = () => (
             </div>
                 <div className="project-right">
                   <p className="work-description">Node.js and MySQL app</p>
-                  <p className="project-link">Github</p>
+                  <p className="project-link"><a target='_blank' rel="noopener noreferrer"href="https://github.com/kendrak922/Amazon-Customer">Github</a></p>
                </div>
             </div>
                     </div>
                             </div>
-
-
                             );
 
 export default Work;
